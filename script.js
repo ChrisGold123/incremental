abb326ab0f1121f44ee35e29f4ea52cda6c6bc5f
ChrisGold <pzukowwski@wp.pl>
2017-04-22 00:01:24 +0200

    fine

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var armor = 0;
 var two = 0;
 var costTwo = 10;
+var prodTwo = 0;
+var incTwo = 1;
 
 function giveOne(number) {
   armor += number;
@@ -11,7 +13,13 @@ function buyTwo() {
   if(armor >= costTwo) {
     two += 1;
     armor -= costTwo;
-    costTwo *= 1.1;
+    costTwo = Math.floor(costTwo * Math.pow(1.1, two);
     document.getElementById('costTwo').innerHTML = costTwo;
+    document.getElementById('armory').innerHTML = armor;
+    document.getElementById('amTwo').innerHTML = two;
   }
 }
+
+setInterval(function() {
+  giveOne(two);
+}, 1000);
