73c9f5fab6cd143ac5914cd3890942bbb16f4a27
ChrisGold <pzukowwski@wp.pl>
2017-04-21 23:39:04 +0200

    v0.02

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 var armor = 0;
+var two = 0;
+var costTwo = 10;
 
-function giveOne() {
-  armor += 1;
+function giveOne(number) {
+  armor += number;
+  document.getElementById('armory').innerHTML = armor;
+}
+
+function buyTwo() {
+  if(armor >= costTwo) {
+    two += 1;
+    armor -= costTwo;
+    costTwo *= 1.1;
+    document.getElementById('costTwo').innerHTML = costTwo;
+  }
 }
